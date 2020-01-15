7024ee5d960a6b41fea3410dec9ee1cf7c1cb85d
Shanda Woods <shandawoods@Shandas-Mini.lan1>
2020-01-14 19:06:34 -0500

    Built users router and posts router.

---
+++ posts/posts-router.js
@@ -0,0 +1,102 @@
+const express = require('express');
+
+const Posts = require('../data/db.js'); // don't forget to import anything needed
+
+const router = express.Router();
+
+// a Router can have middleware
+// a Router can have endpoints
+
+// this router only cares about whatever comes after /
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the posts',
+      });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Posts.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Post not found' });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    });
+});
+
+router.post('/', (req, res) => {
+  Posts.add(req.body)
+    .then(comments => {
+      res.status(201).json(comments);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the comments',
+      });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Posts.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'The post could not be removed',
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    });
+});
+
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+module.exports = router;
+
+// create file for the Router
+// write code to create a router: require('express').Router();
+// export it
+// require and use the router on server.js: server.use('/api/hubs', hubsRouter)
